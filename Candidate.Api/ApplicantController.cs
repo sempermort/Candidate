865c97a01b688798574e5d5f35484b272f13fab8
semperjack@gmail.com <semperjack@gmail.com>
2024-06-09 19:27:54 +0300

    Initial

---
+++ Candidate.Api/ApplicantController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Candidate.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicantController : ControllerBase
+    {
+    }
+}
